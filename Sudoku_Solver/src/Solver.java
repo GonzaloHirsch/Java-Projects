889d976202b24549eb10255324b74a6ef341a9e2
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-01-12 23:12:56 -0300

    Finished Sudoku Solver
    Added a Sudoku Solver with a non-recursive backtracking algorithm

---
+++ Sudoku_Solver/src/Solver.java
@@ -0,0 +1,169 @@
+import java.util.*;
+
+public class Solver {
+    private Number[][] board;
+
+    private HashSet<Integer> squareNumbers = new HashSet<>();
+    private HashSet<Integer> verticalNumbers = new HashSet<>();
+    private HashSet<Integer> horizontalNumbers = new HashSet<>();
+
+    private int squareX;
+    private int squareY;
+
+    private Deque<Number> visitedNumbers = new ArrayDeque<>(81);
+    private Deque<Number> numbersToVisit = new ArrayDeque<>(81);
+
+    public Solver(int[][] board){
+        this.board = new Number[9][9];
+
+        for(int y = 0; y < 9; y++){
+            for(int x = 0; x < 9; x++){
+                this.board[x][y] = new Number(board[x][y], x, y);
+                if (board[x][y] > 0){
+                    this.board[x][y].isFixed = true;
+                } else {
+                    this.board[x][y].isFixed = false;
+                    this.numbersToVisit.add(this.board[x][y]);
+                }
+            }
+        }
+    }
+
+    public int[][] solve(){
+
+        this.backtrackSolve();
+
+        int[][] solution = new int[9][9];
+
+        for(int y = 0; y < 9; y++){
+            for(int x = 0; x < 9; x++){
+                solution[x][y] = this.board[x][y].number;
+            }
+        }
+
+        return solution;
+    }
+
+    private void backtrackSolve(){
+        Number visiting = null;
+        boolean hasToBT = false;
+        boolean isValid = true;
+
+        while (this.numbersToVisit.size() > 0){
+            visiting = this.numbersToVisit.poll();
+            do {
+                visiting.number += 1;
+                isValid = visiting.number <= 9 && this.isValidAddition(visiting.x, visiting.y);
+            } while (!isValid && visiting.number <= 9);
+
+            // Backtrack case
+            if (visiting.number > 9){
+                visiting.number = 0;
+                this.numbersToVisit.addFirst(visiting);
+                visiting = this.visitedNumbers.poll();
+                if (visiting == null) throw new RuntimeException("This Sudoku has no solution!");
+                this.numbersToVisit.addFirst(visiting);
+            } else {
+                this.visitedNumbers.addFirst(visiting);
+            }
+        }
+    }
+
+    private boolean isValidAddition(int x, int y){
+        this.squareX = x / 3;
+        this.squareY = y / 3;
+
+        int squareCeros = 0;
+        int verticalCeros = 0;
+        int horizontalCeros = 0;
+
+        this.squareNumbers.clear();
+        this.verticalNumbers.clear();
+        this.horizontalNumbers.clear();
+
+        for (int ys = this.squareY * 3; ys < this.squareY * 3 + 3; ys++){
+            for (int xs = this.squareX * 3; xs < this.squareX * 3 + 3; xs++){
+                if (this.board[xs][ys].number == 0){
+                    squareCeros++;
+                }
+                else {
+                    this.squareNumbers.add(this.board[xs][ys].number);
+                }
+            }
+        }
+
+        for (int vertical = 0; vertical < 9; vertical++){
+            if (this.board[x][vertical].number == 0){
+                verticalCeros++;
+            }
+            else {
+                this.verticalNumbers.add(this.board[x][vertical].number);
+            }
+        }
+
+        for (int horizontal = 0; horizontal < 9; horizontal++){
+            if (this.board[horizontal][y].number == 0){
+                horizontalCeros++;
+            }
+            else {
+                this.horizontalNumbers.add(this.board[horizontal][y].number);
+            }
+        }
+
+        return (this.squareNumbers.size() + squareCeros) == 9
+                && (this.verticalNumbers.size() + verticalCeros) == 9
+                && (this.horizontalNumbers.size() + horizontalCeros) == 9;
+    }
+
+    public String getPrintableSolution(){
+        StringBuilder sb = new StringBuilder();
+
+        for(int y = 0; y < 9; y++){
+            if (y % 3 == 0)
+                sb.append("+---------+---------+---------+\n");
+            for(int x = 0; x < 9; x++){
+                if (x % 3 != 0)
+                    sb.append(' ');
+                else
+                    sb.append("| ");
+                sb.append(this.board[x][y].number);
+                if (x == 8)
+                    sb.append(" |");
+                else
+                    sb.append(' ');
+            }
+            sb.append('\n');
+            if (y == 8)
+                sb.append("+---------+---------+---------+\n");
+        }
+
+        return sb.toString();
+    }
+
+    private class Number {
+        private int number;
+        private int x;
+        private int y;
+        private boolean isFixed;
+
+        public Number(){
+            this.number = 0;
+            this.x = -1;
+            this.y = -1;
+            this.isFixed = false;
+        }
+
+        public Number(int number, int x, int y, boolean isFixed){
+            this.number = number;
+            this.x = x;
+            this.y = y;
+            this.isFixed = isFixed;
+        }
+
+        public Number(int number, int x, int y){
+            this.number = number;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Sudoku_Solver/src/Tester.java
@@ -0,0 +1,52 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+public class Tester {
+
+    public static void main(String[] args){
+        try {
+            System.out.println("Welcome to Sudoku Solver, input your Sudoku(numbers sepparated by a space and places with no numbers have 0s, input has to be each row continuously):");
+            String input = new Scanner(System.in).nextLine();
+
+            int[][] parsedInput = parseInput(input);
+
+            Solver mySolver = new Solver(parsedInput);
+
+            long start = System.nanoTime();
+
+            mySolver.solve();
+
+            long end = System.nanoTime();
+
+            Duration duration = Duration.ofNanos(end - start);
+
+            System.out.println("The solution is(took " + duration.toMillis() + " millis):");
+            System.out.println(mySolver.getPrintableSolution());
+        } catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int[][] parseInput(String input){
+        String[] numbersInText = input.split(" ");
+        if (numbersInText.length != 81) throw new RuntimeException("Wrong Sudoku input!");
+        int[][] res = new int[9][9];
+
+        int index = 0;
+        int num;
+        for (int y = 0; y < 9; y++){
+            for (int x = 0; x < 9; x++){
+                num = Integer.parseInt(numbersInText[index++]);
+                if (num > 9 || num < 0) throw new RuntimeException("Wrong Sudoku input!");
+                res[x][y] = num;
+            }
+        }
+
+        return res;
+    }
+}
