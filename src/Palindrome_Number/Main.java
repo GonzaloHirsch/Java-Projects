7a8df41ce4a112a88c16e81a20d0e7d69da082db
Gonzalo Hirsch <ghirsch@itba.edu.ar>
2020-05-03 22:58:42 -0300

    Added largest rectangle in histogram and aplindrome number

---
+++ src/Palindrome_Number/Main.java
@@ -0,0 +1,33 @@
+package Palindrome_Number;
+
+// ---------------------------------- SOLVED ----------------------------------
+// --------------------------- FACEBOOK / April 2020 --------------------------
+/**
+ * Original problem: https://leetcode.com/problems/palindrome-number/
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Example 1:
+ *
+ * Input: 121
+ * Output: true
+ * Example 2:
+ *
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ *
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * Follow up:
+ *
+ * Coud you solve it without converting the integer to a string?
+ */
+class Main {
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        sol.isPalindrome(100021);
+    }
+}
